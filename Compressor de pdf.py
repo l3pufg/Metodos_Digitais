cfc827ce1db7d048c738f5217d946fdffc99f829
eduardo2s <easilva91@gmail.com>
2017-06-27 11:58:03 -0300

    Adicionando comentários

--- Compressor de pdf.py
+++ Compressor de pdf.py
@@ -1,5 +1,22 @@
+#Instale o Ghostscript / install ghostscript first
+from __future__ import print_function
 import os
+import subprocess
 
-for fich in os.listdir('.'):
-        if fich[-3:]=="pdf":
-                os.system("ps2pdf -dPDFSETTINGS=/ebook %s reduc/%s" % (fich,fich))
+for root, dirs, files in os.walk("pasta com os arquivos / folder with files"):
+    for file in files:
+        if file.endswith(".pdf"):
+            filename = os.path.join(root, file)
+            print (filename)
+            arg1= '-sOutputFile=' + "c" +  file #added a c to the filename
+            p = subprocess.Popen(['C:/Program Files (x86)/gs/gs9.21/bin/gswin32c.exe', #path to the ghostscript
+                                  '-sDEVICE=pdfwrite', 
+                                  '-dCompatibilityLevel=1.5',
+                                  '-sstdout=%stderr',
+                                  '-dGrayImageResolution=600',
+                                  '-dMonoImageResolution=1200',
+                                  '-dColorImageResolution=300',
+                                  '-dPDFSETTINGS=/screen', '-dNOPAUSE', 
+                                  '-dBATCH', '-dQUIET', str(arg1), filename], 
+                                 stdout=subprocess.PIPE)
+            print (p.communicate())
