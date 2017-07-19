3b1c1b215d3cfcb786ea2342b368157a519fa9d7
eduardo2s <easilva91@gmail.com>
2017-07-19 20:44:49 -0300

    Atualizado - sem stopwords

--- nuvem_de_palavras.py
+++ nuvem_de_palavras.py
@@ -2,26 +2,32 @@
 """
 Created on Tue Jul 18 19:01:15 2017
 
+Script que gera nuvem de palavras retirando as stopwords
+
 @author: eduardo
 """
-# assim como próprio python esse script apresenta erros em relação #
-# a caracteres especiais como â, ã, ç, ê... entre outros a não ser #
-# que salve o arquivo que for utilizado em ANSCI ao invés de UTF-8 #
-
 import numpy as np # importa o numpy
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt # biblioteca para gráficos
+import nltk # importa o nltk
+import string
+from nltk.corpus import stopwords # bilbioteca de stopwords dos corpus do nltk
 from wordcloud import WordCloud # biblioteca que cria nuvens de palavras
 
-# Abre o arquivo (nuvem3.txt) e lê, como esté sendo usado o pandas ele lê automáticamente
-f = open('nuvem3.txt', "rt")
-text = f.readlines()
+# Abre o arquivo e verifica as Stopwords, caso identifique são retiradas
+# com o NLTK e o pacote de lingua portuguesa
+with open('your_file.txt', 'r') as f, open('your_new_file_without_stopwords.txt','w') as outnew:
+	for line in f.readlines():
+	    print(" ".join([word for word in line.lower().translate(str.maketrans('', '', string.punctuation)).split() 
+        	if len(word) >=4 and word not in stopwords.words('portuguese')]), file=outnew)
 
-# Cria um data frame com o pandas e dá o título
+# Abre o novo arquivo gerado e cria um dataframe com o pandas
+infile = open('your_new_file_without_stopwords.txt', 'r')
+text = infile.readlines()
 df = pd.DataFrame({'palavras_texto':text})
 
 # Em caso de textos separado por linhas com (enter) ele apresenta as 5 primeiras
-# Em caso de paragráfo apresenta só uma parte da primeira linha
+# em caso de paragráfo apresenta só uma parte da primeira linha
 print(df.head(5))
 
 # gera uma figura de tamanho 12 por 12 cm, para plotar as palavras
@@ -38,6 +44,5 @@ plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis('off')
 # apresenta a imagem gerada
 plt.show()
-
 # existe uma infinidade de variações #
 # fonte/source: https://github.com/amueller/word_cloud #
